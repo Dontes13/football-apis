cb7d8ac9e09f732ee5a1c91831a3019ffbd1bf7e
Dante Santurian <dantesan@umich.edu>
2026-06-18 19:31:32 +0000

    loading api into database - PL standings 2025 season

--- work.py
+++ work.py
@@ -1,4 +1,7 @@
 import requests
+import pandas as pd
+import sqlalchemy as db
+from pprint import pprint
 
 API_KEY = 'bb56c97b593c449a8bbbe4c46802a239'
 
@@ -7,4 +10,24 @@ headers = {'X-Auth-Token': API_KEY}
 url = 'https://api.football-data.org/v4/competitions/PL/standings'
 
 response = requests.get(url, headers=headers)
-print(response.json())
+data = response.json()
+competition_year = data['filters']['season']
+
+for item in data['standings'][0]['table']:
+  team_name = item['team']['name']
+  item['team'] = team_name
+
+#pprint(data['standings'][0]['table'])
+#exit()
+
+df = pd.DataFrame(data['standings'][0]['table'])
+
+
+engine = db.create_engine('sqlite:///football.db')
+df.to_sql('standings', con=engine, if_exists='replace', index=False)
+
+with engine.connect() as connection:
+  query = "SELECT * FROM standings;"
+  query_result = connection.execute(db.text(query)).fetchall()
+  print(f"Season Start: {competition_year}")
+  print(pd.DataFrame(query_result))
